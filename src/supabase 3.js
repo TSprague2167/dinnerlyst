867f3ff64f48239af0fbab0edebf3e3508f9e06d
Tawney Sprague <tawneyknavel@gmail.com>
2026-05-10 21:44:19 -0600

    Fix deployment files

--- src/supabase 3.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://qjlrqgefocsputtstgdl.supabase.co'
-const supabaseKey = sb_publishable_EYLK1iarZoRp2tMia2TB0A_gxtzp2CZ
-const supabase = createClient(supabaseUrl, supabaseKey)
